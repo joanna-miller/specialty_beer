58f46d5708133059cc143994fb6dc02a7fb62b5d
Jo <joannadawnmiller@gmail.com>
2021-03-19 16:55:55 -0400

    pass test to titleize product name

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all 
+    @products = Product.all.order('brand', 'name')
     render :index
   end
 
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,5 +7,8 @@ describe Product do
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
   it { should validate_presence_of :brand }
-
+  it('titleizes the name of a product') do
+    beer = Product.create({name: "fake beer", brand: "Dogfish Head", cost: "10.99", country_of_origin: "USA"})
+    expect(beer.name()).to(eq('Fake Beer'))
+  end
 end
