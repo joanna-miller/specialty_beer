cdbb11b0e169fe6d7097af06a3a13918763d12a4
Jo <joannadawnmiller@gmail.com>
2021-03-19 12:56:42 -0400

    pass rating validation for review

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
   validates :author, presence: true 
   validates :content_body, presence: true 
   validates_length_of :content_body, minimum: 50, maximum: 250
-  validates :rating, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
