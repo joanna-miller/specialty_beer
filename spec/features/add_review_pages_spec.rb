5b84eea1d5e8c771c83c79e1056f2df82a6e978e
Jo <joannadawnmiller@gmail.com>
2021-03-19 16:15:21 -0400

    pass integrated tests for adding review

---
+++ spec/features/add_review_pages_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe "the add a review process" do
+  it "adds a new review" do
+    new_beer = Product.new({name: "Namaste", brand: "Dogfish Head", cost: "10.99", country_of_origin: "USA"})
+    new_beer.save
+    visit product_path(new_beer)
+    click_link 'Leave a Review'
+    fill_in 'review_author', :with => 'Jo Miller'
+    fill_in 'review_content_body', :with => 'Lorem ipsum dolor sit amet, consectetuer adipiscing'
+    fill_in 'review_rating', :with => '5'
+    click_on 'Create Review'
+    expect(page).to have_content 'Written by: Jo Miller'
+  end
+
+  it "gives an error when any field is left blank" do
+    new_beer = Product.new({name: "Namaste", brand: "Dogfish Head", cost: "10.99", country_of_origin: "USA"})
+    new_beer.save
+    visit product_path(new_beer)
+    click_on 'Leave a Review'
+    click_on 'Create Review'
+    expect(page).to have_content "Author can't be blank"
+    expect(page).to have_content "Content body can't be blank"
+    expect(page).to have_content "Rating can't be blank"
+  end
+end
--- spec/features/edit_product_pages_spec.rb
+++ spec/features/edit_product_pages_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "the edit a product process" do
   it "edits an existing product" do
-    new_beer = Product.new({name: "90 Minute IPA", brand: "Dogfish Head", cost: "10.99", country_of_origin: "USA"})
+    new_beer = Product.new({name: "Namaste", brand: "Dogfish Head", cost: "10.99", country_of_origin: "USA"})
     new_beer.save
     visit product_path(new_beer)
     click_link 'Edit Beer'
@@ -10,5 +10,4 @@ describe "the edit a product process" do
     click_on 'Update Product'
     expect(page).to have_content 'Dogfish Head: 120 Minute IPA'
   end
-
 end
