3718e1c6207547acb1903e6222f523575ee44ebe
Jo <joannadawnmiller@gmail.com>
2021-03-19 12:47:32 -0400

    pass test to validate length of review content_body

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,6 @@ class Review < ApplicationRecord
   belongs_to :product
   validates :author, presence: true 
   validates :content_body, presence: true 
+  validates_length_of :content_body, minimum: 50, maximum: 250
   validates :rating, presence: true
 end
