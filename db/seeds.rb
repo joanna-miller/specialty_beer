b74a67d69d81e86822c6a5b0618ac3fcf98d338e
Jo <joannadawnmiller@gmail.com>
2021-03-20 09:39:47 -0400

    update price range for beer

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ Product.destroy_all
 Review.destroy_all
 
 50.times do |index|
-  product = Product.create!(name: Faker::Beer.unique.name, brand: Faker::Beer.brand, country_of_origin: Faker::Address.country, cost: Faker::Commerce.price(range: 0..100.0))
+  product = Product.create!(name: Faker::Beer.unique.name, brand: Faker::Beer.brand, country_of_origin: Faker::Address.country, cost: Faker::Commerce.price(range: 7..25.0))
   5.times do |index|
     Review.create!(author: Faker::FunnyName.two_word_name, content_body: Faker::Lorem.paragraph_by_chars(number: 200, supplemental: false), rating: Faker::Number.between(from: 1, to: 5), product_id: product.id)
   end
