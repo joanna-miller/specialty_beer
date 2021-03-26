903dcee403c3abffca717a2b6ad06e4c93302249
Jo <joannadawnmiller@gmail.com>
2021-03-26 19:15:30 -0400

    give admin ability to change user admin status

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  before_action do
+    redirect_to new_user_session_path unless current_user && current_user.admin
+  end
+
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def show
+  @user = User.find(params[:id])
+  render :show
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path 
+    end     
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:admin)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users
+  resources :users, only: [:index, :show, :update]
   resources :products do
     resources :reviews, only: [:new, :create]
   end
